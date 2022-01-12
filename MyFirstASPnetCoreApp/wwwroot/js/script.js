39ea17e1cdfdc46ae6fb650624b6d07cbabd5a71
mattcperry382 <mattcperry382@gmail.com>
2022-01-12 00:22:46 -0700

    Add project files.

---
+++ MyFirstASPnetCoreApp/wwwroot/js/script.js
@@ -0,0 +1,13 @@
+﻿// document.getElementById('btnSend').addEventListener('click', () => {
+//     alert('From: ' + document.getElementById('txtFrom').value +
+//         ", Subject: " + document.getElementById('txtSubject').value +
+//         ", Message: " + document.getElementById('txtMessage').value);
+// })
+
+$('#btnSend').click(() => {
+    alert('From: ' + $('#txtFrom').val() +
+        ", Subject: " + $('#txtSubject').val() +
+        ", Message: " + $('#txtMessage').val());
+
+    $('#picGanderson').fadeOut('slow')
+})
